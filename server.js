a7482a3d7031b3a057a2c4086c663cac1d223c52
Anahita <shakofa.mahmody@gmail.com>
2021-03-23 19:33:17 -0700

    Add code to set up the express app to handle the files. on server.js file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes =require('./routes/htmlRoutes');
 
 
 
 const app = express();
 const PORT =process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static
+app.use(express.static('public'));
+//routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => console.log(`listening to PORT: ${PORT}`));
