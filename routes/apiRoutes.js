12c6e01959aa6dd05e757395db4206ba6d303afd
Anahita <shakofa.mahmody@gmail.com>
2021-03-24 19:37:43 -0700

    Correct capitalizations

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,29 @@
 
-const store = require('./db/store');
+const store = require('../db/store');
 const router = require('express').Router();
 
 
 router.get('/notes', (req, res) => {
     store
-    getNote().then((notes) => {return res.json(notes);
-    }).catch((err) => res.status(500).json(err));
-
-});
-
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+  });
 
 
 router.post('/notes', (req, res) => {
     store
-    addNote().then((notes) => {return res.json(notes);
-    }).catch((err) => res.status(500).json(err));
+    .addNote(req.body).then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
 });
 
 
 //deleting with id
 router.delete('/notes/:id', (req, res) => {
     store
-    deleteNote(req.params.id).then(() => res.json({ok: true})).catch((err) => res.status(500).json(err));
+    .deleteNote(req.params.id).then(() => res.json({ok: true})).catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
